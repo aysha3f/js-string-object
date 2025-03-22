6aeb2c642fb1c3be643874f8eb8911cf99735b30
aysha3f <tahiaysha220@gmail.com>
2025-03-23 03:02:22 +0600

    object

---
+++ object.js
@@ -0,0 +1,19 @@
+
+// non-primitive
+// object
+const bottle = {
+    brand: 'Apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+const subject = {
+name: 'biology',
+teacher: 'Rasheda',
+examDate: '30 Feb',
+chapters: ['first','second','third' ],
+exam: {
+    name: 'Final Exam',
+    marks: 100
+}
+}
