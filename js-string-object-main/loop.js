2de35d3f1d75c880e539b5fc366239e21b035c50
aysha3f <tahiaysha220@gmail.com>
2025-03-23 22:04:40 +0600

    first file task complete

--- js-string-object-main/loop.js
+++ js-string-object-main/loop.js
@@ -6,20 +6,17 @@ const mobile = {
     isNew: true
 }
 
-
-// for of: array
-// for in: object
-
-for (const prop in mobile){
+// for of : array
+// for in : object
+for(const prop in mobile){
     // console.log(prop)
-     console.log(mobile[prop])
+    // console.log(mobile[prop])
 }
-
 
 const keys = Object.keys(mobile);
 console.log(keys);
 
-for (const key of keys){
-  
-   console.log(key,':', mobile[key])
+for(const key of keys){
+    console.log(key, ':', mobile[key]);
+    
 }
