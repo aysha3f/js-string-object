195eae4c439d86a7dc82677d2aba39d3d508e319
aysha3f <tahiaysha220@gmail.com>
2025-03-23 11:58:11 +0600

    property

---
+++ property.js
@@ -0,0 +1,23 @@
+const person = {
+    name: 'sodor uddin',
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: true,
+   'fav places':['bandorban','saintmatin', 'kuakata']
+}
+// console.log(person);
+
+// dot notation
+// console.log(person.profession);
+const income = person.salary;
+// console.log(income);
+
+
+// bracket notation
+// console.log(person['age'])
+const boyos = person['age']
+// console.log(boyos
+//     )
+
+console.log(person['fav places'])
